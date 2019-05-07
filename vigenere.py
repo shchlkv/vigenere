61f812ff4989e88af7870927832a701af809a6e2
A.S <shchlkv@gmail.com>
2019-05-07 20:04:02 +0300

    It is first version which work

--- vigenere.py
+++ vigenere.py
@@ -8,8 +8,8 @@ get index of letter from ALPHABET
 import math
 import sys
 alphabet=list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
-#keyword=list('бетономешалкавйъчыгыхеаюукй')               #password or keyword
-#word=list('безинформатикинетникчемноесуществованиеэтогомирабудущегоиникодганебудетмирбезмашинэтонемирэтопостноесуществованиекогдаянапочтеслужилямщикоомА')
+keyword=list('бетономешалкавйъчыгыхеаюукй')               #password or keyword
+word=list('безинформатикинетникчемноесуществованиеэтогомирабудущегоиникодганебудетмирбезмашинэтонемирэтопостноесуществование')
 
 indexes_of_keyword=[]           # list, which will to contain indices of keyword
 indexes_of_word=[]              # list, which will to contain indices of word
@@ -17,22 +17,18 @@ crypt_word=[]
 indexes_of_cript_word=[]
 decrypt_word=[]
 
-word=input('Enter any word or phrase wich need to encrypt. The phrase should be without spaces and with only cyrillic letters: ')
-keyword=input('Enter keyword to encrypt the phrase: ')
-
-
 for i in range(len(keyword)):
         if keyword[i] in alphabet:          #cheking of letters from list 'keyword' with index 'i' in list 'alphabet'
                 letter=keyword[i]           #this variable 'letter' contains letter from list 'keyword' with index 'i'. 
                 index_of_letter_from_list_alphabet=alphabet.index(letter) #this variable contains index of letter from 'alphabet' list
                 indexes_of_keyword.append(index_of_letter_from_list_alphabet)  # insert 'index_of_letter_from_list_alphabet' in  list 'indexes_of_keyword' 
-             
+               
 for i in range(len(word)):
         if word[i] in alphabet:         #cheking of letters from list 'word' with index 'i' in list 'alphabet'
                 letter=word[i]          #this variable 'letter' contains letter from list 'word' with index 'i'.
                 index_of_letter_from_list_alphabet=alphabet.index(letter) #this variable contains index of letter from 'alphabet' list
                 indexes_of_word.append(index_of_letter_from_list_alphabet)        # insert 'index_of_letter_from_list_alphabet' in  list 'indexes_of_keyword' 
-       
+
 
 # calculation of cryptword
 n=len(word)
@@ -54,10 +50,10 @@ print(''.join(crypt_word))
 
 
 for i in range(len(word)):
-        if crypt_word[i] in alphabet:         
-                letter=crypt_word[i]          
-                index_of_letter_from_list_alphabet=alphabet.index(letter) 
-                indexes_of_cript_word.append(index_of_letter_from_list_alphabet)  
+        if crypt_word[i] in alphabet:         #cheking of letters from list 'word' with index 'i' in list 'alphabet'
+                letter=crypt_word[i]          #this variable 'letter' contains letter from list 'word' with index 'i'.
+                index_of_letter_from_list_alphabet=alphabet.index(letter) #this variable contains index of letter from 'alphabet' list
+                indexes_of_cript_word.append(index_of_letter_from_list_alphabet)        # insert 'index_of_letter_from_list_alphabet' in  list 'indexes_of_keyword' 
 
 n=len(word)
 
@@ -72,7 +68,7 @@ while n > 0:
         j+=1
         if i==len(keyword):
                 i=0
-print('Расшифрованное слово: ')
+
 print(''.join(decrypt_word))
 
 
