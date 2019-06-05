25f03c56e9d35de9daae77626fcac7a45678a978
A.S <shchlkv@gmail.com>
2019-06-05 18:36:23 +0300

    шифрование работает, зашифрованная фраза записывается в файл, добавлены комментарии

--- vigenere.py
+++ vigenere.py
@@ -9,8 +9,8 @@ get index of letter from ALPHABET
 убрать из него пробелы и знаки препинания и лат.буквы, если есть
 зашифровываем и пишем в отдельный файл
 vigenere_file_text.txt - исходный файл с текстом
-vigenere_file_encrypt.txt - зашифрованный файл
-vigenere_file_decrypt.txt - расшифрованный файл
+vigenere_file_encrypt.txt - зашифрованный текст
+vigenere_file_decrypt.txt - расшифрованный текст
 
 '''
 
@@ -18,7 +18,9 @@ vigenere_file_decrypt.txt - расшифрованный файл
 # -*- coding: utf-8 -*-
 import math
 import sys
+
 alphabet=list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+
 #keyword=list('бетономешалкавйъчыгыхеаюукй')               #password or keyword
 #word=list('безинформатикинетникчемноесуществованиеэтогомирабудущегоиникодганебудетмирбслужилямщикоом')
 #word=[]
@@ -32,7 +34,7 @@ print('your file must to contain text or phrase or word wich you want to encrypt
 #word=input('Enter any word or phrase wich need to encrypt. The phrase should be without spaces and with only cyrillic letters: ')
 keyword=input('Enter keyword to encrypt the phrase: ')
 
-vigenere_file_decrypt=open('/home/ark/vigenere_file_decrypt.txt', 'w')
+
 vigenere_file_encrypt=open('/home/ark/vigenere_file_encrypt.txt', 'w')
 
 
@@ -40,59 +42,122 @@ for i in range(len(keyword)):
         if keyword[i] in alphabet:          #cheking of letters from list 'keyword' with index 'i' in list 'alphabet'
                 letter=keyword[i]           #this variable 'letter' contains letter from list 'keyword' with index 'i'. 
                 index_of_letter_from_list_alphabet=alphabet.index(letter) #this variable contains index of letter from 'alphabet' list
-                #print(index_of_letter_from_list_alphabet)
                 indexes_of_keyword.append(index_of_letter_from_list_alphabet)  # insert 'index_of_letter_from_list_alphabet' in  list 'indexes_of_keyword' 
              
 #надо сделать так: читать строки из файла в список, а дальше работать со списком, как это уже было.
 
+'''
+этот блок открывает файл vigenere_file_text.txt, читает из него и присваивает переменной word считанный текст
+'''
+
 vigenere_file_text=open('/home/ark/vigenere_file_text.txt', 'r')
 for line in vigenere_file_text:
-#        counter_of_letters_in_text_file=len(letter)
-#        print(len(letter))
-#        print(line)
         word=line #читаем стороку из файла и присваиваем ее списку word
-#print(word)
-#print(len(word))
+
+'''
+Читаем каждую букву word (то, что считано из файла vigenere_file_text.txt), вычисляем  индекс каждой в списке alhabet,
+добавляем каждый вычесленный индекс в список indexes_of_word
+'''
 
 for i in range(len(word)):
         if word[i] in alphabet:         #cheking of letters from list 'word' with index 'i' in list 'alphabet'
                 letter=word[i]          #this variable 'letter' contains letter from list 'word' with index 'i'.
                 index_of_letter_from_list_alphabet=alphabet.index(letter) #this variable contains index of letter from 'alphabet' list
                 indexes_of_word.append(index_of_letter_from_list_alphabet)        # insert 'index_of_letter_from_list_alphabet' in  list 'indexes_of_keyword' 
-               # print(index_of_letter_from_list_alphabet)
+#               print(index_of_letter_from_list_alphabet)
 
-# calculation of cryptword
-n=len(word) 
-print(n)
+'''
+j=0
+for i in range(len(word)-1):
+        print('i = ',i)
+        print('j = ',j)
+        new_index_letter=(indexes_of_keyword[i]+indexes_of_word[j])%33
+        new_cript_letter=alphabet[new_index_letter]
+        crypt_word.append(new_cript_letter)
+        j+=1
+        if i==len(keyword):
+                i=0
+'''
+
+# этот блок шифрует фразу из файла vigenere_file_text.txt 
+
+n=len(word) # количество букв в файле vigenere_file_text.txt
 i=0
 j=0
-while n > 0:
-        new_index_letter=(indexes_of_keyword[i]+indexes_of_word[j])%33
-       # print(new_index_letter)
-        new_cript_letter=alphabet[new_index_letter]
-      #  print(new_cript_letter)
-        crypt_word.append(new_cript_letter)
+while n > 1:
+        new_index_letter=(indexes_of_keyword[i]+indexes_of_word[j])%33 # вычисляем индекс зашифрованной буквы
+        new_cript_letter=alphabet[new_index_letter] # находим по индексу букву в списке алфавит
+        crypt_word.append(new_cript_letter) # добавляем уже зашифрованную букву в конец слова
         n-=1
         i+=1
         j+=1
         if i==len(keyword):
                 i=0
+
+#открываем файл
+vigenere_file_decrypt=open('/home/ark/vigenere_file_decrypt.txt', 'w')
+#пишем в файл vigenere_file_decrypt.txt зашифрованное слово crypt_word
+vigenere_file_encrypt.write(''.join(crypt_word)) # список crypt_word надо преобразовать в строковое значение, т.к. списки в файл не записываются
+#закрываем файл, в который записали зашифрованное слово                
+vigenere_file_encrypt.close() 
+
 '''
 print(''.join(word))
 print(''.join(keyword))
 print(''.join(crypt_word))
 '''
 
+
+'''
+СЛЕДУЮЩИЙ БЛОК - ЭТО РАСШИФРОВКА КАК СЛОВА ИЗ СПИСКА crypt_word, ТАК И ИЗ ФАЙЛА vigenere_file_decrypt.txt
+'''
+#откроем файл на чтение
+vigenere_file_encrypt=open('/home/ark/vigenere_file_encrypt.txt', 'r')
+
+for line in vigenere_file_encrypt:
+        word=line #читаем стороку из файла и присваиваем ее списку word
+
+        
+#print(word)
+
+
+# этот блок расшифрует текст из файла vigenere_file_encrypt.txt 
+
+'''
+Ищем тут ошибку
+'''
+
+
+n=len(word) # количество букв в файле vigenere_file_encrypt.txt
+i=0
+j=0
+while n > 1:
+        new_index_letter_decrypt=(indexes_of_cript_word[j]-indexes_of_keyword[i]+33)%33
+        new_decript_letter=alphabet[new_index_letter_decrypt]
+        decrypt_word.append(new_decript_letter)
+        n-=1
+        i+=1
+        j+=1
+        if i==len(keyword):
+                i=0
+
+                
+#print('i == ',i)
+#i=0
+
+# зашифрованное слово ...
+
+
 for i in range(len(word)):
-        if crypt_word[i] in alphabet:         
+        if crypt_word[i] in alphabet:   
                 letter=crypt_word[i]          
                 index_of_letter_from_list_alphabet=alphabet.index(letter) 
-                indexes_of_cript_word.append(index_of_letter_from_list_alphabet)  
+                indexes_of_cript_word.append(index_of_letter_from_list_alphabet)  # добавляем индекс буквы из списка алфавит в список  indexes_of_cript_word
 
 n=len(word)
 i=0
 j=0
-while n > 0:
+while n > 1:
         new_index_letter_decrypt=(indexes_of_cript_word[j]-indexes_of_keyword[i]+33)%33
         new_decript_letter=alphabet[new_index_letter_decrypt]
         decrypt_word.append(new_decript_letter)
@@ -108,5 +173,4 @@ print('Расшифрованное слово: ')
 print(''.join(decrypt_word))
 '''
 vigenere_file_text.close()
-vigenere_file_encrypt.close()
 vigenere_file_decrypt.close() 
